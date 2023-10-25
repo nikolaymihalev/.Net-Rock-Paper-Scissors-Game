bcd5cd95133ccb60cb77ec2fa259090a05c02259
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-10-25 09:48:11 +0000

    Player Choice Logic

--- Code/RockPaperScissors/RockPaperScissors/Program.cs
+++ Code/RockPaperScissors/RockPaperScissors/Program.cs
@@ -10,3 +10,38 @@ Console.WriteLine("Enter your choice!");
 Console.WriteLine("For rock - rock/r");
 Console.WriteLine("For paper - paper/p");
 Console.WriteLine("For scissors - scissors/s");
+
+var choice=Console.ReadLine();
+if (choice.ToLower() == "rock" || choice.ToLower() == "r")
+{
+    playerChoice = Rock;
+}
+else if (choice.ToLower() == "paper" || choice.ToLower() == "p")
+{
+    playerChoice = Paper;
+}
+else if (choice.ToLower() == "scissors" || choice.ToLower() == "s")
+{
+    playerChoice = Scissors;
+}
+else
+{
+    while (playerChoice == String.Empty)
+    {
+        Console.WriteLine("Wrong input!");
+        Console.WriteLine("Enter your choice again!");
+        choice=Console.ReadLine();
+        if (choice.ToLower() == "rock" || choice.ToLower() == "r")
+        {
+            playerChoice = Rock;
+        }
+        else if (choice.ToLower() == "paper" || choice.ToLower() == "p")
+        {
+            playerChoice = Paper;
+        }
+        else if (choice.ToLower() == "scissors" || choice.ToLower() == "s")
+        {
+            playerChoice = Scissors;
+        }
+    }
+}
